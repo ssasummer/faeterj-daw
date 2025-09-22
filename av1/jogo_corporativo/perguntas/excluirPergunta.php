8871d103e21aa6abf51a05c4c0e8146ae589cdd5
Patrícia Sá <116208299+ssasummer@users.noreply.github.com>
2025-09-22 13:25:04 -0300

    Create excluirPergunta.php

---
+++ av1/jogo_corporativo/perguntas/excluirPergunta.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Excluir Pergunta</title>
+</head>
+<body>
+    <h1>Excluir Pergunta</h1>
+
+    <?php
+    $id = $_GET["id"];
+    $perguntas = file("../perguntas.txt");
+    unset($perguntas[$id]);
+    file_put_contents("../perguntas.txt", implode("", $perguntas));
+    echo "<p>Pergunta excluida. </p>";
+    ?>
+    <p><a href="../index.php">inicio</a></p>
+</body>
+</html>
