91d089f4d5bdb72c73803702dc67213d15ba9660
Patrícia Sá <116208299+ssasummer@users.noreply.github.com>
2025-09-27 17:06:28 -0300

    Update excluirPergunta.php

--- av1/jogo_corporativo/perguntas/excluirPergunta.php
+++ av1/jogo_corporativo/perguntas/excluirPergunta.php
@@ -4,17 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Excluir Pergunta</title>
+    <link rel = "stylesheet" href = "../style.css">
 </head>
 <body>
-    <h1>Excluir Pergunta</h1>
-
-    <?php
-    $id = $_GET["id"];
-    $perguntas = file("../perguntas.txt");
-    unset($perguntas[$id]);
-    file_put_contents("../perguntas.txt", implode("", $perguntas));
-    echo "<p>Pergunta excluida. </p>";
-    ?>
-    <p><a href="../index.php">inicio</a></p>
+    <div>
+        <h1>Excluir Pergunta</h1>
+        <?php
+            $id = $_GET["id"];
+            $perguntas = file("../perguntas.txt");
+            unset($perguntas[$id]);
+            file_put_contents("../perguntas.txt", implode("", $perguntas));
+            echo "<p>Pergunta excluida. </p>";
+        ?>
+        <p><a href="../index.php">inicio</a></p>
+    </div>
 </body>
 </html>
