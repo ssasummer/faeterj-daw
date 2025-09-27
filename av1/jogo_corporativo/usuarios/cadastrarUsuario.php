6b4a848d5a4204a1e9076cb0abb00c31a6c1d7ea
Patrícia Sá <116208299+ssasummer@users.noreply.github.com>
2025-09-27 17:12:52 -0300

    Update cadastrarUsuario.php

--- av1/jogo_corporativo/usuarios/cadastrarUsuario.php
+++ av1/jogo_corporativo/usuarios/cadastrarUsuario.php
@@ -4,29 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastrar Usuario</title>
+    <link rel = "stylesheet" href = "../style.css">
 </head>
 <body>
-    <h1>Cadastrar Usuario</h1>
-
-    <form method="post" action="">
-        Nome: <input type="text" name="nome"><br><br>
-        Email: <input type="email" name="email"><br><br>
-        Senha: <input type="password" name="senha"><br><br>
-        <input type="submit" value="Cadastrar">
-    </form>
-
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $nome = $_POST["nome"];
-        $email = $_POST["email"];
-        $senha = $_POST["senha"];
-
-        $linha = $nome . ";" . $email . ";" . $senha . "\n";
-        file_put_contents("../usuarios.txt", $linha, FILE_APPEND);
-
-        echo "<p>Usuario cadastrado com sucesso!</p>";
-    }
-    ?>
-    <p><a href="../index.php"> inicio </a></p>
+    <div>
+        <h1>Cadastrar Usuario</h1>
+        <form method="post" action="">
+            Nome: <input type="text" name="nome"><br><br>
+            Email: <input type="email" name="email"><br><br>
+            Senha: <input type="password" name="senha"><br><br>
+            <input type="submit" value="Cadastrar">
+        </form>
+    
+        <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $nome = $_POST["nome"];
+                $email = $_POST["email"];
+                $senha = $_POST["senha"];
+        
+                $linha = $nome . ";" . $email . ";" . $senha . "\n";
+                file_put_contents("../usuarios.txt", $linha, FILE_APPEND);
+        
+                echo "<p>Usuario cadastrado com sucesso!</p>";
+            }
+        ?>
+        <p><a href="../index.php"> inicio </a></p>
+    </div>
 </body>
 </html>
