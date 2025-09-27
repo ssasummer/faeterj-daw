afbc911830abf31040ba6a16733ea7319ec9887e
Patrícia Sá <116208299+ssasummer@users.noreply.github.com>
2025-09-27 17:09:44 -0300

    Update listarUmaPergunta.php

--- av1/jogo_corporativo/perguntas/listarUmaPergunta.php
+++ av1/jogo_corporativo/perguntas/listarUmaPergunta.php
@@ -4,19 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ver Pergunta</title>
+    <link rel = "stylesheet" href = "../style.css">
 </head>
 <body>
-    <h1>Detalhes da Pergunta</h1>
-
-    <?php
-    $id = $_GET["id"];
-    $perguntas = file("../perguntas.txt");
-    $dados = explode(";", trim($perguntas[$id]));
-
-    echo "<p><b>Pergunta:</b> $dados[0]</p>";
-    echo "<p><b>Tipo:</b> $dados[1]</p>";
-    echo "<p><b>Respostas:</b> $dados[2]</p>";
-    ?>
-    <p><a href="../index.php">inicio</a></p>
+   <div>
+        <h1>Detalhes da Pergunta</h1>
+        <?php
+            $id = $_GET["id"];
+            $perguntas = file("../perguntas.txt");
+            $dados = explode(";", trim($perguntas[$id]));
+        
+            echo "<p><b>Pergunta:</b> $dados[0]</p>";
+            echo "<p><b>Tipo:</b> $dados[1]</p>";
+            echo "<p><b>Respostas:</b> $dados[2]</p>";
+        ?>
+        <p><a href="../index.php">inicio</a></p>
+   </div>
 </body>
 </html>
