c605d7ddcb70965bdb87424464283cc345e5b2f6
Patrícia Sá <116208299+ssasummer@users.noreply.github.com>
2025-11-03 14:44:41 -0300

    Create incluir.php

---
+++ atividades/nov_03/incluir.php
@@ -0,0 +1,16 @@
+<?php include 'db.php'; if(isset($_POST['nome'])){ $nome=$_POST['nome']; $matricula=$_POST['matricula']; $email=$_POST['email']; $conn->query("INSERT INTO alunos VALUES('$matricula','$nome','$email')"); header("Location: index.php"); } ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Adicionar Aluno</title>
+  </head>
+<body>
+  <h2>Adicionar Aluno</h2>
+  <form method="POST">
+    Matrícula: <input type="text" name="matricula" required><br>
+    Nome: <input type="text" name="nome" required><br>
+    Email: <input type="email" name="email" required><br>
+  <button type="submit">Salvar</button>
+</form>
+</body>
+</html>
